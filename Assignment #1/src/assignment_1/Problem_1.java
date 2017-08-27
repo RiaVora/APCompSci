8e4f4389ab6a33bf4a98e1c8efa23934111199b9
RiaVora <31297758+RiaVora@users.noreply.github.com>
2017-08-27 14:16:04 -0700

    Assignment #1
    Put all files from Eclipse Assignment #1 into GitHub Repository APCompSci

---
+++ Assignment #1/src/assignment_1/Problem_1.java
@@ -0,0 +1,20 @@
+package assignment_1;
+
+public class Problem_1 {
+	public static void main (String[] args) {
+		pln(ret() + "");
+	}
+	
+	public static int ret () {
+		return 17;
+	}
+	
+	public static void p (String args) {
+		System.out.print(args);
+	}
+
+	public static void pln (String args) {
+		System.out.println(args);
+	}
+
+}
